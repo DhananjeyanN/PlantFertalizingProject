7cf681e1b3110cc3484bbf40c02065532f701004
dhananjeyannatarajan <potatoess1256@gmail.com>
2023-07-22 22:35:57 -0700

    new

--- Core/views.py
+++ Core/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from Core.forms import AddPlantForm
 from Core.models import Plant, DataTable
 from accounts.models import SiteProfile
@@ -82,41 +82,56 @@ def add_plant(request):
     return render(request, 'add_plant.html', context)
 
 
-def plant_details(request):
+def plant_details(request, plant_id):
     profile = SiteProfile.objects.all().first()
-    plants = Plant.objects.filter(user=request.user)
-    plant_names = [plant.name.replace(" ", "_") for plant in plants]
+    plant = get_object_or_404(Plant, pk= plant_id)
+    plant_dict = {}
     detail_data = {}
     last_24_hours_ago = datetime.now() - timedelta(hours=500)
     latest_data = []
-    for plant in plants:
-        plant_dict = {}
-        plant_data = DataTable.objects.filter(plant=plant, date_time__gte=last_24_hours_ago)
-        latest_ec_data = DataTable.objects.filter(plant=plant).exclude(m_ec__isnull=True).latest('date_time')
-        plant_dict['latest_ec'] = latest_ec_data.m_ec
-        plant_dict['latest_ph'] = latest_ec_data.m_ph
-        plant_dict['latest_npk'] = latest_ec_data.m_npk
-        plant_dict['latest_temp'] = latest_ec_data.m_temp
-        plant_dict['latest_moist'] = latest_ec_data.m_moist
-        plant_dict['name'] = plant.name
-        latest_data.append(plant_dict)
-        data_key = plant.name.replace(" ", "_")
-        # For more detailed data
-        detail_data[data_key] = {}
-        detail_data[data_key]['date_time'] = [plant_data_item.date_time.isoformat() for plant_data_item in plant_data]
-        detail_data[data_key]['m_temp'] = [plant_data_item.m_temp for plant_data_item in plant_data]
-        detail_data[data_key]['m_ec'] = [plant_data_item.m_ec for plant_data_item in plant_data]
-        detail_data[data_key]['m_npk'] = [plant_data_item.m_npk for plant_data_item in plant_data]
-        detail_data[data_key]['m_ph'] = [plant_data_item.m_ph for plant_data_item in plant_data]
-    plant_ids = [plant.id for plant in plants]
-    print('DETAIL DATA')
-    print(detail_data)
+    plant_data = DataTable.objects.filter(plant=plant, date_time__gte=last_24_hours_ago)
+    latest_ec_data = DataTable.objects.filter(plant=plant).exclude(m_ec__isnull=True).latest('date_time')
+    plant_dict['latest_ec'] = latest_ec_data.m_ec
+    plant_dict['latest_ph'] = latest_ec_data.m_ph
+    plant_dict['latest_npk'] = latest_ec_data.m_npk
+    plant_dict['latest_temp'] = latest_ec_data.m_temp
+    plant_dict['latest_moist'] = latest_ec_data.m_moist
+    plant_dict['name'] = plant.name
+    print(plant_dict,'PLANT DICT')
+    # For more detailed data
 
-    print('LATEST DATA')
-    print(latest_data)
-    plant_temperatures = [plant.temperature for plant in plants]
-    context = {'plants': plants, 'names': plant_names, 'temps': plant_temperatures, 'profile': profile,
-               'latest_data': latest_data, 'detail_data': json.dumps(detail_data),
-               "ids": json.dumps(plant_ids),
-               }
+    detail_data = {}
+    plant_fields = ['m_temp', 'm_ec', 'm_npk', 'm_ph']
+    r_date_time = [plant_data_item.date_time.isoformat() for plant_data_item in plant_data]
+    formatted_timestamps = []
+
+    for ts in r_date_time:
+        dt = datetime.fromisoformat(ts.replace("Z", "+00:00"))
+        dt_dict = {
+            'year': dt.year,
+            'month': dt.month,
+            'day': dt.day,
+            'hour': dt.hour,
+            'minute': dt.minute,
+        }
+        formatted_timestamps.append(dt_dict)
+        datetime_hours = [t['hour'] for t in formatted_timestamps]
+        print(datetime_hours, 'DATETIME HOURS')
+    print(formatted_timestamps, 'FORMATTED TIME STAMPS')
+    detail_data['m_temp'] = [plant_data_item.m_temp for plant_data_item in plant_data]
+    detail_data['m_ec'] = [plant_data_item.m_ec for plant_data_item in plant_data]
+    detail_data['m_npk'] = [plant_data_item.m_npk for plant_data_item in plant_data]
+    detail_data['m_ph'] = [plant_data_item.m_ph for plant_data_item in plant_data]
+    print(detail_data, 'DETAIL DATA')
+    print(r_date_time, 'RECORDED DATE TIME')
+    context={
+        'latest_data':json.dumps(plant_dict),
+        'detail_data':json.dumps(detail_data),
+        'r_date_time':r_date_time,
+        'p_fields':plant_fields,
+        'plant':plant,
+        'profile': profile,
+        'f_time': json.dumps(formatted_timestamps),
+        'hours': datetime_hours
+    }
     return render(request, 'Core/reg_home_pop_up.html', context=context)
